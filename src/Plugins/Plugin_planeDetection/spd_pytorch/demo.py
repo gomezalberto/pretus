61c1cc54910d29523e01e8887fdf0235da24eb83
Alberto Gomez <alberto.gomez@kcl.ac.uk>
2021-08-04 09:22:51 +0100

    corrected an issue with the demo in plane detect

--- src/Plugins/Plugin_planeDetection/spd_pytorch/demo.py
+++ src/Plugins/Plugin_planeDetection/spd_pytorch/demo.py
@@ -3,7 +3,7 @@ import cv2
 from utils.util import json_file_to_pyobj
 from skimage.transform import resize
 import SimpleITK as sitk
-import worker
+import standardplanedetection_worker
 
 def image_loader_sitk(image_name, image_size):
     """load image, returns cuda tensor"""
@@ -26,14 +26,14 @@ if __name__ == '__main__':
 
 
 
-    worker.initialize('./','ifind2_net_Jan15.pth')
+    standardplanedetection_worker.initialize('./','ifind2_net_Jan15.pth')
 
     # Load Testing image
     image = image_loader_sitk(im_file, [224, 288])
 
-    output = worker.dowork(image, verbose=False)
+    output = standardplanedetection_worker.dowork(image, verbose=False)
 
-    label_names = worker.getlabels()
+    label_names = standardplanedetection_worker.getlabels()
     id = np.argmax(output)
 
     print('Predicted {}'.format(label_names[id]))
